070b92eefd12afa628a47f8a64927a86c719b447
francesco <francescoenzoalloro@gmail.com>
2025-06-27 16:19:53 +0200

    Fix: Correzione del comando di release per usare python3

--- init_db.py
+++ init_db.py
@@ -11,7 +11,14 @@ def initialize_database():
     print("INIT_DB: Inizio inizializzazione database.")
 
     with app.app_context():
-        # ... (le sezioni 1 e 2 per creare Dipartimenti e Corsi di Laurea rimangono invariate) ...
+        try:
+            db.create_all()
+            print("INIT_DB: Struttura tabelle SQL verificata/creata.")
+        except Exception as e:
+            print(f"ERRORE INIT_DB (SQL): {e}")
+            raise
+
+        print("INIT_DB: Verificando e aggiungendo dati SQL...")
         
         # --- 1. Popolamento Dipartimenti ---
         departments_to_ensure = ['DING', 'DST', 'DEMM']
@@ -19,48 +26,46 @@ def initialize_database():
             if not Department.query.filter_by(name=dept_name).first():
                 db.session.add(Department(name=dept_name))
         db.session.commit()
-        print("INIT_DB: Dipartimenti assicurati.")
 
         # --- 2. Popolamento Corsi di Laurea Triennale ---
         ding_dept = Department.query.filter_by(name='DING').one()
         dst_dept = Department.query.filter_by(name='DST').one()
         demm_dept = Department.query.filter_by(name='DEMM').one()
         
+        # MODIFICA: Nomi resi coerenti, semplici e minuscoli
         degree_programs_to_ensure = [
+            # DING
             {'name': 'Ingegneria Energetica', 'department': ding_dept},
             {'name': 'Ingegneria Civile', 'department': ding_dept},
             {'name': 'Ingegneria Informatica', 'department': ding_dept},
             {'name': 'Ingegneria Biomedica', 'department': ding_dept},
-            {'name': 'Scienze Biologiche', 'department': dst_dept},
-            {'name': 'Biotecnologie', 'department': dst_dept},
-            {'name': 'Scienze Naturali', 'department': dst_dept},
-            # NOTA: Uso ancora il nome lungo per matchare il tuo codice attuale
-            {'name': 'Scienze Motorie per lo Sport e la Salute', 'department': dst_dept},
+        
+            # DST
+            {'name': 'scienze_biologiche', 'department': dst_dept},
+            {'name': 'biotecnologie', 'department': dst_dept},
+            {'name': 'scienze_naturali', 'department': dst_dept},
+            {'name': 'scienze_motorie', 'department': dst_dept},
         ]
         for dp_data in degree_programs_to_ensure:
             if not DegreeProgram.query.filter_by(name=dp_data['name']).first():
                 db.session.add(DegreeProgram(name=dp_data['name'], department=dp_data['department']))
         db.session.commit()
-        print("INIT_DB: Corsi di Laurea assicurati.")
-
 
         # --- 3. Popolamento Esami ---
         try:
-            print("INIT_DB: Inizio popolamento Esami.")
+            # MODIFICA: Usiamo i nuovi nomi coerenti per recuperare gli oggetti
             programs = {
                 'Ingegneria Energetica': DegreeProgram.query.filter_by(name='Ingegneria Energetica').one(),
                 'Ingegneria Civile': DegreeProgram.query.filter_by(name='Ingegneria Civile').one(),
                 'Ingegneria Informatica': DegreeProgram.query.filter_by(name='Ingegneria Informatica').one(),
                 'Ingegneria Biomedica': DegreeProgram.query.filter_by(name='Ingegneria Biomedica').one(),
-                'Scienze Biologiche': DegreeProgram.query.filter_by(name='Scienze Biologiche').one(),
-                'Biotecnologie': DegreeProgram.query.filter_by(name='Biotecnologie').one(),
-                'Scienze Naturali': DegreeProgram.query.filter_by(name='Scienze Naturali').one(),
-                'Scienze Motorie per lo Sport e la Salute': DegreeProgram.query.filter_by(name='Scienze Motorie per lo Sport e la Salute').one(),
+                'scienze_biologiche': DegreeProgram.query.filter_by(name='scienze_biologiche').one(),
+                'biotecnologie': DegreeProgram.query.filter_by(name='biotecnologie').one(),
+                'scienze_naturali': DegreeProgram.query.filter_by(name='scienze_naturali').one(),
+                'scienze_motorie': DegreeProgram.query.filter_by(name='scienze_motorie').one(),
             }
-            print("INIT_DB: Dizionario 'programs' caricato correttamente.")
 
             courses_to_add = [
-                # ... (tutta la tua lista di corsi rimane qui, invariata)
                 # Ingegneria Energetica
                 ('Analisi Matematica I', 1, programs['Ingegneria Energetica']), ('Analisi Matematica II', 1, programs['Ingegneria Energetica']),('Fisica Generale I', 1, programs['Ingegneria Energetica']),('Fisica Generale II', 1, programs['Ingegneria Energetica']), ('Algebra Lineare Geometria e ricerca operativa', 1, programs['Ingegneria Energetica']), ('Chimica', 1, programs['Ingegneria Energetica']), ('Elementi di Informatica', 1, programs['Ingegneria Energetica']), ('Fondamenti della misurazione', 1, programs['Ingegneria Energetica']),('Inglese', 1, programs['Ingegneria Energetica']),
                 ('Modelli di Reattori Chimici', 2, programs['Ingegneria Energetica']),('Sistemi Elettrici per L^Energia', 2, programs['Ingegneria Energetica']),('Termofluido Dinamica e Trasmissione del Calore', 2, programs['Ingegneria Energetica']), ('Meccanica Applicata alle Macchine', 2, programs['Ingegneria Energetica']), ('Elettrotecnica', 2, programs['Ingegneria Energetica']), ('Fisica Tecnica', 2, programs['Ingegneria Energetica']),('Processi di Combustione', 2, programs['Ingegneria Energetica']),('Macchine a Fluido', 2, programs['Ingegneria Energetica']),
@@ -81,50 +86,40 @@ def initialize_database():
                 ('Misure Elettroniche', 2, programs['Ingegneria Biomedica']), ('Elementi di Biochimica', 2, programs['Ingegneria Biomedica']), ('Elettrotecnica', 2, programs['Ingegneria Biomedica']), ('Elettronica', 2, programs['Ingegneria Biomedica']),('Matematica II', 2, programs['Ingegneria Biomedica']),('Sistemi Dinamici', 2, programs['Ingegneria Biomedica']),('Probabilità e segnali', 2, programs['Ingegneria Biomedica']),('Elaborazione Numerica dei segnali', 2, programs['Ingegneria Biomedica']),
                 ('BioElettromagnetismo', 3, programs['Ingegneria Biomedica']),('Sistemi di Acquisizione Dati', 3, programs['Ingegneria Biomedica']),('Sistemi Biomedicali', 3, programs['Ingegneria Biomedica']),('Laboratorio di BioElettronica', 3, programs['Ingegneria Biomedica']),('Laboratorio di Misure Elettroniche per Applicazioni Medicali', 3, programs['Ingegneria Biomedica']),
                 
+                # MODIFICA: Usiamo le nuove chiavi per associare i corsi
                 # Scienze Biologiche
-                ('BIOLOGIA E SISTEMATICA VEGETALE', 1, programs['Scienze Biologiche']), ('CHIMICA GENERALE', 1, programs['Scienze Biologiche']), ('CHIMICA ORGANICA', 1, programs['Scienze Biologiche']), ('CITOLOGIA ED ISTOLOGIA', 1, programs['Scienze Biologiche']), ('ENGLISH FOR BIOLOGICAL SCIENCES', 1, programs['Scienze Biologiche']), ('FISICA ED ELEMENTI DI MATEMATICA PER LA BIOLOGIA', 1, programs['Scienze Biologiche']), ('FONDAMENTI DI INFORMATICA', 1, programs['Scienze Biologiche']), ('LABORATORIO DI BIOLOGIA DI BASE', 1, programs['Scienze Biologiche']),
-                ('BIOCHIMICA', 2, programs['Scienze Biologiche']), ('BIOLOGIA MOLECOLARE', 2, programs['Scienze Biologiche']), ('FISIOLOGIA GENERALE', 2, programs['Scienze Biologiche']), ('GENETICA', 2, programs['Scienze Biologiche']), ('MICROBIOLOGIA GENERALE', 2, programs['Scienze Biologiche']), ('ZOOLOGIA', 2, programs['Scienze Biologiche']),
-                ('ECOLOGIA', 3, programs['Scienze Biologiche']), ('EMBRIOLOGIA E ANATOMIA COMPARATA', 3, programs['Scienze Biologiche']), ('FARMACOLOGIA', 3, programs['Scienze Biologiche']), ('FISIOLOGIA VEGETALE', 3, programs['Scienze Biologiche']), ('LABORATORIO DI BIOLOGIA SPERIMENTALE', 3, programs['Scienze Biologiche']),
+                ('BIOLOGIA E SISTEMATICA VEGETALE', 1, programs['scienze_biologiche']), ('CHIMICA GENERALE', 1, programs['scienze_biologiche']), ('CHIMICA ORGANICA', 1, programs['scienze_biologiche']), ('CITOLOGIA ED ISTOLOGIA', 1, programs['scienze_biologiche']), ('ENGLISH FOR BIOLOGICAL SCIENCES', 1, programs['scienze_biologiche']), ('FISICA ED ELEMENTI DI MATEMATICA PER LA BIOLOGIA', 1, programs['scienze_biologiche']), ('FONDAMENTI DI INFORMATICA', 1, programs['scienze_biologiche']), ('LABORATORIO DI BIOLOGIA DI BASE', 1, programs['scienze_biologiche']),
+                ('BIOCHIMICA', 2, programs['scienze_biologiche']), ('BIOLOGIA MOLECOLARE', 2, programs['scienze_biologiche']), ('FISIOLOGIA GENERALE', 2, programs['scienze_biologiche']), ('GENETICA', 2, programs['scienze_biologiche']), ('MICROBIOLOGIA GENERALE', 2, programs['scienze_biologiche']), ('ZOOLOGIA', 2, programs['scienze_biologiche']),
+                ('ECOLOGIA', 3, programs['scienze_biologiche']), ('EMBRIOLOGIA E ANATOMIA COMPARATA', 3, programs['scienze_biologiche']), ('FARMACOLOGIA', 3, programs['scienze_biologiche']), ('FISIOLOGIA VEGETALE', 3, programs['scienze_biologiche']), ('LABORATORIO DI BIOLOGIA SPERIMENTALE', 3, programs['scienze_biologiche']),
 
                 # Biotecnologie
-                ('BIOLOGIA CELLULARE', 1, programs['Biotecnologie']), ('BIOTECNOLOGIE E DIRITTO DELL UNIONE EUROPEA', 1, programs['Biotecnologie']), ('CHIMICA GENERALE E INORGANICA (Biotecnologie)', 1, programs['Biotecnologie']), ('CHIMICA ORGANICA (Biotecnologie)', 1, programs['Biotecnologie']), ('FISICA CON LABORATORIO', 1, programs['Biotecnologie']), ('GENETICA (Biotecnologie)', 1, programs['Biotecnologie']), ('LINGUA INGLESE', 1, programs['Biotecnologie']), ('MATEMATICA', 1, programs['Biotecnologie']),
-                ('BIOCHIMICA (Biotecnologie)', 2, programs['Biotecnologie']), ('BIOLOGIA APPLICATA', 2, programs['Biotecnologie']), ('BIOLOGIA MOLECOLARE (Biotecnologie)', 2, programs['Biotecnologie']), ('CHIMICA FISICA', 2, programs['Biotecnologie']), ('MICROBIOLOGIA', 2, programs['Biotecnologie']), ('PRINCIPI DI BIOINFORMATICA', 2, programs['Biotecnologie']), ('STATISTICA', 2, programs['Biotecnologie']),
-                ('BIOTECNOLOGIE INDUSTRIALI', 3, programs['Biotecnologie']), ('CARATTERIZZAZIONE STRUTTURALE DEI COMPOSTI CHIMICI', 3, programs['Biotecnologie']), ('FARMACOLOGIA E TOSSICOLOGIA', 3, programs['Biotecnologie']), ('FITOCHIMICA E SUE APPLICAZIONI BIOTECNOLOGICHE', 3, programs['Biotecnologie']), ('LABORATORI INTEGRATI', 3, programs['Biotecnologie']),
+                ('BIOLOGIA CELLULARE', 1, programs['biotecnologie']), ('BIOTECNOLOGIE E DIRITTO DELL UNIONE EUROPEA', 1, programs['biotecnologie']), ('CHIMICA GENERALE E INORGANICA (Biotecnologie)', 1, programs['biotecnologie']), ('CHIMICA ORGANICA (Biotecnologie)', 1, programs['biotecnologie']), ('FISICA CON LABORATORIO', 1, programs['biotecnologie']), ('GENETICA (Biotecnologie)', 1, programs['biotecnologie']), ('LINGUA INGLESE', 1, programs['biotecnologie']), ('MATEMATICA', 1, programs['biotecnologie']),
+                ('BIOCHIMICA (Biotecnologie)', 2, programs['biotecnologie']), ('BIOLOGIA APPLICATA', 2, programs['biotecnologie']), ('BIOLOGIA MOLECOLARE (Biotecnologie)', 2, programs['biotecnologie']), ('CHIMICA FISICA', 2, programs['biotecnologie']), ('MICROBIOLOGIA', 2, programs['biotecnologie']), ('PRINCIPI DI BIOINFORMATICA', 2, programs['biotecnologie']), ('STATISTICA', 2, programs['biotecnologie']),
+                ('BIOTECNOLOGIE INDUSTRIALI', 3, programs['biotecnologie']), ('CARATTERIZZAZIONE STRUTTURALE DEI COMPOSTI CHIMICI', 3, programs['biotecnologie']), ('FARMACOLOGIA E TOSSICOLOGIA', 3, programs['biotecnologie']), ('FITOCHIMICA E SUE APPLICAZIONI BIOTECNOLOGICHE', 3, programs['biotecnologie']), ('LABORATORI INTEGRATI', 3, programs['biotecnologie']),
 
                 # Scienze Naturali
-                ('CHIMICA GENERALE E INORGANICA (Scienze Naturali)', 1, programs['Scienze Naturali']), ('CHIMICA ORGANICA CON ELEMENTI DI BIOCHIMICA', 1, programs['Scienze Naturali']), ('FONDAMENTI DI BIOLOGIA', 1, programs['Scienze Naturali']), ('FONDAMENTI DI SCIENZE DELLA TERRA', 1, programs['Scienze Naturali']), ('GEOCHIMICA ED ANALISI DEI DATI AMBIENTALI', 1, programs['Scienze Naturali']), ('INGLESE (Scienze Naturali)', 1, programs['Scienze Naturali']), ('MATEMATICA E STATISTICA', 1, programs['Scienze Naturali']),
-                ('FISICA CON LABORATORIO (Scienze Naturali)', 2, programs['Scienze Naturali']), ('GEOMORFOLOGIA', 2, programs['Scienze Naturali']), ('IDROGEOLOGIA', 2, programs['Scienze Naturali']), ('MINERALOGIA', 2, programs['Scienze Naturali']), ('PALEONTOLOGIA, PALEOECOLOGIA E LABORATORIO', 2, programs['Scienze Naturali']), ('PETROGRAFIA', 2, programs['Scienze Naturali']), ('ZOOLOGIA GENERALE E SISTEMATICA', 2, programs['Scienze Naturali']), ('BOTANICA GENERALE E SISTEMATICA', 2, programs['Scienze Naturali']),
-                ('ANATOMIA COMPARATA', 3, programs['Scienze Naturali']), ('ECOLOGIA (Scienze Naturali)', 3, programs['Scienze Naturali']), ('GENETICA (Scienze Naturali)', 3, programs['Scienze Naturali']), ('GEOFISICA DELLA TERRA SOLIDA', 3, programs['Scienze Naturali']), ('SOSTENIBILITÀ AMBIENTALE E PROTEZIONE DELLA NATURA', 3, programs['Scienze Naturali']),
+                ('CHIMICA GENERALE E INORGANICA (Scienze Naturali)', 1, programs['scienze_naturali']), ('CHIMICA ORGANICA CON ELEMENTI DI BIOCHIMICA', 1, programs['scienze_naturali']), ('FONDAMENTI DI BIOLOGIA', 1, programs['scienze_naturali']), ('FONDAMENTI DI SCIENZE DELLA TERRA', 1, programs['scienze_naturali']), ('GEOCHIMICA ED ANALISI DEI DATI AMBIENTALI', 1, programs['scienze_naturali']), ('INGLESE (Scienze Naturali)', 1, programs['scienze_naturali']), ('MATEMATICA E STATISTICA', 1, programs['scienze_naturali']),
+                ('FISICA CON LABORATORIO (Scienze Naturali)', 2, programs['scienze_naturali']), ('GEOMORFOLOGIA', 2, programs['scienze_naturali']), ('IDROGEOLOGIA', 2, programs['scienze_naturali']), ('MINERALOGIA', 2, programs['scienze_naturali']), ('PALEONTOLOGIA, PALEOECOLOGIA E LABORATORIO', 2, programs['scienze_naturali']), ('PETROGRAFIA', 2, programs['scienze_naturali']), ('ZOOLOGIA GENERALE E SISTEMATICA', 2, programs['scienze_naturali']), ('BOTANICA GENERALE E SISTEMATICA', 2, programs['scienze_naturali']),
+                ('ANATOMIA COMPARATA', 3, programs['scienze_naturali']), ('ECOLOGIA (Scienze Naturali)', 3, programs['scienze_naturali']), ('GENETICA (Scienze Naturali)', 3, programs['scienze_naturali']), ('GEOFISICA DELLA TERRA SOLIDA', 3, programs['scienze_naturali']), ('SOSTENIBILITÀ AMBIENTALE E PROTEZIONE DELLA NATURA', 3, programs['scienze_naturali']),
             
-                
                 # Scienze Motorie
-                ('Anatomia Umana', 1, programs['Scienze Motorie per lo Sport e la Salute']), ('Biologia applicata', 1, programs['Scienze Motorie per lo Sport e la Salute']), ('Biochimica (Scienze Motorie)', 1, programs['Scienze Motorie per lo Sport e la Salute']), ('Fisica con elementi di biomeccanica', 1, programs['Scienze Motorie per lo Sport e la Salute']), ('Principi di diritto e management dello sport', 1, programs['Scienze Motorie per lo Sport e la Salute']), ('Inglese scientifico', 1, programs['Scienze Motorie per lo Sport e la Salute']), ('Teoria del movimento e tecnica dell\'attività motoria e sportiva', 1, programs['Scienze Motorie per lo Sport e la Salute']), ('Pedagogia e sociologia della comunicazione', 1, programs['Scienze Motorie per lo Sport e la Salute']),
-                ('Informatica e Statistica', 2, programs['Scienze Motorie per lo Sport e la Salute']), ('Metodologie didattiche per le attivita\' sportive', 2, programs['Scienze Motorie per lo Sport e la Salute']), ('Didattica e pedagogia speciale', 2, programs['Scienze Motorie per lo Sport e la Salute']), ('Fisiologia umana applicata alle scienze motorie', 2, programs['Scienze Motorie per lo Sport e la Salute']), ('Teoria dell\'allenamento e metodi di valutazione motoria', 2, programs['Scienze Motorie per lo Sport e la Salute']), ('Bioingegneria applicata alle scienze motorie', 2, programs['Scienze Motorie per lo Sport e la Salute']),
-                ('Basi di nutrizione applicata allo sport', 3, programs['Scienze Motorie per lo Sport e la Salute']), ('Igiene', 3, programs['Scienze Motorie per lo Sport e la Salute']), ('Genetica e performance sportiva', 3, programs['Scienze Motorie per lo Sport e la Salute']), ('Patologia generale', 3, programs['Scienze Motorie per lo Sport e la Salute']), ('Farmacologia applicata allo sport', 3, programs['Scienze Motorie per lo Sport e la Salute']),
+                ('Anatomia Umana', 1, programs['scienze_motorie']), ('Biologia applicata', 1, programs['scienze_motorie']), ('Biochimica (Scienze Motorie)', 1, programs['scienze_motorie']), ('Fisica con elementi di biomeccanica', 1, programs['scienze_motorie']), ('Principi di diritto e management dello sport', 1, programs['scienze_motorie']), ('Inglese scientifico', 1, programs['scienze_motorie']), ('Teoria del movimento e tecnica dell\'attività motoria e sportiva', 1, programs['scienze_motorie']), ('Pedagogia e sociologia della comunicazione', 1, programs['scienze_motorie']),
+                ('Informatica e Statistica', 2, programs['scienze_motorie']), ('Metodologie didattiche per le attivita\' sportive', 2, programs['scienze_motorie']), ('Didattica e pedagogia speciale', 2, programs['scienze_motorie']), ('Fisiologia umana applicata alle scienze motorie', 2, programs['scienze_motorie']), ('Teoria dell\'allenamento e metodi di valutazione motoria', 2, programs['scienze_motorie']), ('Bioingegneria applicata alle scienze motorie', 2, programs['scienze_motorie']),
+                ('Basi di nutrizione applicata allo sport', 3, programs['scienze_motorie']), ('Igiene', 3, programs['scienze_motorie']), ('Genetica e performance sportiva', 3, programs['scienze_motorie']), ('Patologia generale', 3, programs['scienze_motorie']), ('Farmacologia applicata allo sport', 3, programs['scienze_motorie']),
             ]
             
-            print(f"INIT_DB: Trovati {len(courses_to_add)} corsi da aggiungere/verificare.")
-            
-            # Aggiunta corsi con commit finale
             for name, year, program_obj in courses_to_add:
                 if not Course.query.filter_by(name=name, degree_program_id=program_obj.id).first():
                     db.session.add(Course(name=name, year=year, degree_program=program_obj))
             
-            print("INIT_DB: Tutti i corsi sono stati aggiunti alla sessione. Inizio commit.")
             db.session.commit()
-            print("INIT_DB: Popolamento esami SQL completato con successo.")
-
+            print("INIT_DB: Popolamento esami SQL completato.")
         except Exception as e:
             db.session.rollback()
-            # Stampa un errore molto più visibile
-            print("\n" + "="*50)
-            print("ERRORE CRITICO in INIT_DB durante il popolamento degli esami.")
-            print(f"ECCEZIONE: {e}")
-            print("La transazione è stata annullata (rollback).")
-            print("="*50 + "\n")
+            print(f"ERRORE INIT_DB (SQL Popolamento Esami): {e}")
             raise
 
-    # ... (Sezione MongoDB invariata) ...
+    # --- Sezione MongoDB (invariata) ---
     print("INIT_DB: Inizio operazione su MongoDB...")
     mongo_client = None
     try:
